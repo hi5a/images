40a7360c3dc5a2dd1568d15c74f1559c842c422a
Andrew <andrew@nordicgamelab.org>
2025-06-19 17:43:44 -0400

    cloudflare

---
+++ src/index.js
@@ -0,0 +1,43 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request) {
+		console.log(request)
+		function getQueryPram(key){
+			if (request.url.includes("?")){
+				const query = request.url.split("?");
+				const queries = query[1].split('&');
+				queries.forEach(q => {
+					const kv = q.split('=');
+					if (kv[0] == key){
+						return kv[1]
+					}
+				});
+			}else{
+				return null;
+			}
+		}
+		if (request.url.includes("/img/social/")){
+			let dir = "/img/social/"
+			if (getQueryPram('dark') == 'true'){
+				dir += "dark/"
+			} else{if(getQueryPram('dark') == 'false'){
+				dir += "light/"
+			}}
+			const fileQ = request.url.split('/social/');
+			const file = dir + fileQ[1].split("?")[0];
+			const url = "https://cdn.aball.dev" + file;
+			return await fetch(url)
+		}
+		const newResponse = new Response()
+		return newResponse;
+	},
+  };
